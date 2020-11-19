1a5fcc2d6add3374fd5ccb967515fd9509b25496
JefferyAS <jeffery2001@163.com>
2020-11-19 11:48:35 -0500

    Change testCommit

--- Assets/TestCommit.cs
+++ Assets/TestCommit.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TestCommit : MonoBehaviour
 {
+    public int test;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = 1;
     }
 
     // Update is called once per frame
