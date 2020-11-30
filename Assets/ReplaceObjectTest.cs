9f7d6cc7e1469ac2c90074898875aa8046c2ab57
JefferyAS <jeffery2001@163.com>
2020-11-30 16:09:31 -0500

    Script Updating Nov.30

---
+++ Assets/ReplaceObjectTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceObjectTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CreateCard(GameObject card) {
+        GameObject gameObj =Instantiate(card, transform);
+        gameObj.transform.localPosition = new Vector3(0,0,0);
+    }
+}
---
+++ Assets/ResultText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultText : MonoBehaviour
+{
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void winText(string win) {
+        text.text = win;
+    }
+    public void loseText(string lose) {
+        text.text = lose;
+    }
+    public void tieText(string tie) {
+        text.text = tie;
+    }
+}
