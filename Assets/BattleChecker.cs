3738862be0893dfce8bb01b55c03ae753173da31
JefferyAS <jeffery2001@163.com>
2020-12-13 11:39:47 -0500

    update BattleSceneManage script

--- Assets/BattleChecker.cs
+++ Assets/BattleChecker.cs
@@ -8,12 +8,30 @@ public class BattleChecker : MonoBehaviour
     public GameObject choseCard;
     public GameObject choseEnemy;
     public ResultText resultText;
+    public GameObject textBoard;
     public Text youScoreBoard;
     public Text enemyScoreBoard;
+    public GameObject icon;
+    public GameObject defeatIcon;
 
-    private int youScore;
-    private int enemyScore;
-    
+    public bool isYourTurn=true;
+    public Text youTurn;
+    public Text enemyTurn;
+    //public Sprite[] iconSprites;
+
+    public int youScore;
+    public int enemyScore;
+
+    private float endTurnTimer;
+    public float endTurnSetTimer=3f;
+    private bool isEndTurn;
+
+    public bool enemyResult;
+
+    //result buttons
+    public GameObject winButton;
+    public GameObject loseButton;
+    public GameObject tieButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +43,37 @@ public class BattleChecker : MonoBehaviour
     {
         youScoreBoard.text = youScore.ToString();
         enemyScoreBoard.text = enemyScore.ToString();
+
+        if (endTurnTimer > 0f && isEndTurn) {
+            endTurnTimer -= Time.deltaTime;
+        }
+        else if (endTurnTimer<=0 &&isEndTurn) {
+            isEndTurn = false;
+            EndTurn();
+        }
+
+
+        //show result buttons
+        if (isYourTurn)
+        {
+            if (youScore > enemyScore &&enemyResult)
+            {
+                winButton.SetActive(true);
+            }
+            else if (enemyResult && youScore == enemyScore)
+            {
+                tieButton.SetActive(true);
+            }
+            else if (youScore < enemyScore)
+            {
+                loseButton.SetActive(true);
+            }
+        }
+        else {
+            winButton.SetActive(false);
+            loseButton.SetActive(false);
+            tieButton.SetActive(false);
+        }
     }
     public void ChooseCard(GameObject card) {
         choseCard = card;
@@ -33,22 +82,52 @@ public class BattleChecker : MonoBehaviour
         if (choseCard!=null) {
             choseEnemy = card;
             CardInBattle cardInBattle = choseCard.GetComponent<CardInBattle>();
-            string result=cardInBattle.CompareCard(choseEnemy.GetComponent<EnemyCardScript>());
-            if (result == "win")
+            EnemyCardScript enemy = choseEnemy.GetComponent<EnemyCardScript>();
+            string[] result=cardInBattle.CompareCard(enemy);
+            //Debug.Log(result[1]);
+            if (result[0] == "win")
             {
-                resultText.winText(cardInBattle.winText);
+                enemy.isDefeated = true;
+                CreateIcon(enemy.cardName,result[1]);
+                textBoard.SetActive(true);
+                resultText.winText(cardInBattle.cardName, enemy.cardName);
                 youScore += 1;
+                Instantiate(defeatIcon, choseEnemy.transform.position, Quaternion.identity, choseEnemy.transform);
             }
-            else if (result == "lose")
+            else if (result[0] == "lose")
             {
-                resultText.loseText(cardInBattle.LoseText);
+                CreateIcon(cardInBattle.cardName, result[1]);
+                textBoard.SetActive(true);
+                resultText.loseText(cardInBattle.cardName, enemy.cardName);
                 enemyScore += 1;
+                Instantiate(defeatIcon, choseCard.transform.position, Quaternion.identity, choseCard.transform);
             }
             else {
-                resultText.tieText(cardInBattle.Tie);
+                textBoard.SetActive(true);
+                resultText.tieText(cardInBattle.cardName, enemy.cardName);
             }
-            choseCard = null;
-            choseEnemy = null;
+            isYourTurn = false;
+            EndTurn(0);
         }
+    }
+    public void CreateIcon(string cardName, string element) {
+        GameObject avatar = GameObject.Find(cardName);
+        GameObject icon=Instantiate(this.icon,avatar.transform.position,Quaternion.identity,avatar.transform);
+        icon.GetComponent<ElementIconScript>().SetIcon(element);
+    }
+
+    public void EnemyWin() {
+        enemyScore += 1;
+    }
+    private void EndTurn() {
+        choseCard = null;
+        choseEnemy = null;
+        youTurn.enabled = false;
+        enemyTurn.enabled = true;
+        GetComponent<EnemyAi>().StartTurn();
+    }
+    private void EndTurn(int time) {
+        isEndTurn = true;
+        endTurnTimer = endTurnSetTimer;
     }
 }
--- Assets/BattleInitialize.cs
+++ Assets/BattleInitialize.cs
@@ -7,16 +7,20 @@ public class BattleInitialize : MonoBehaviour
     public DeckTracker deckTracker;
     public GameObject[] placeHolder;
     public GameObject avatarHolder;
+    public GameObject[] activeCardList;
+    public EnemyAi enemyAi;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject[] cardList = deckTracker.GetCardList();
+        activeCardList = new GameObject[cardList.Length];
         for (int i=0;i<cardList.Length;i++)
         {
-            placeHolder[i].GetComponent<ReplaceObjectTest>().CreateCard(cardList[i]);
+            activeCardList[i]=placeHolder[i].GetComponent<ReplaceObjectTest>().CreateCard(cardList[i]);
         }
         avatarHolder.GetComponent<ReplaceObjectTest>().CreateCard(deckTracker.avatar);
+        enemyAi.SetActiveCardList(activeCardList);
     }
 
     // Update is called once per frame
