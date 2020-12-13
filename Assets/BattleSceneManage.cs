8455d2dde93a794197b4d0cd9893933380883c2b
JefferyAS <jeffery2001@163.com>
2020-12-13 11:40:10 -0500

    create battle scene manage script

---
+++ Assets/BattleSceneManage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneManage : MonoBehaviour
+{
+    public GameObject battleScene;
+    public GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowBattleScene() {
+        GameObject newScene=Instantiate(battleScene,canvas.transform.position,Quaternion.identity,canvas.transform);
+        battleScene.SetActive(true);
+        battleScene = newScene;
+    }
+    public void ChangeBattleScene(GameObject scene)
+    {
+        battleScene = scene;
+        GameObject newScene = Instantiate(battleScene, canvas.transform.position, Quaternion.identity, canvas.transform);
+        battleScene.SetActive(true);
+        battleScene = newScene;
+    }
+}
